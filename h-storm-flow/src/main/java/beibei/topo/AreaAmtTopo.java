e4e230865f6bba1ab01130204b77776c7bf6e56f
seventeen17 <1529068093@qq.com>
2017-12-19 17:09:42 +0800

    commit

---
+++ h-storm-flow/src/main/java/beibei/topo/AreaAmtTopo.java
@@ -0,0 +1,40 @@
+package beibei.topo;
+
+import beibei.spout.OrderBaseSpout;
+import kafka.productor.KafkaProperties;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.topology.TopologyBuilder;
+
+
+public class AreaAmtTopo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TopologyBuilder builder = new TopologyBuilder();
+
+		builder.setSpout("spout", new OrderBaseSpout(KafkaProperties.Order_topic), 5);
+
+		Config conf = new Config() ;
+		conf.setDebug(false);
+
+		if (args.length > 0) {
+			try {
+				StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			LocalCluster localCluster = new LocalCluster();
+			localCluster.submitTopology("topology", conf, builder.createTopology());
+		}
+		
+		
+	}
+
+}
