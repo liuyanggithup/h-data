406e7d509b45ba17b35a11896d2a7b32470bcb77
seventeen17 <1529068093@qq.com>
2017-12-21 10:51:57 +0800

    commit

--- h-storm-flow/src/main/java/beibei/topo/AreaAmtTopo.java
+++ h-storm-flow/src/main/java/beibei/topo/AreaAmtTopo.java
@@ -14,35 +14,33 @@ import org.apache.storm.tuple.Fields;
 
 public class AreaAmtTopo {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		TopologyBuilder builder = new TopologyBuilder();
+        TopologyBuilder builder = new TopologyBuilder();
 
-		builder.setSpout("spout", new OrderBaseSpout(KafkaProperties.Order_topic), 5);
-		//随机分组
-		builder.setBolt("filter", new AreaFilterBolt() , 5).shuffleGrouping("spout") ;
-		//按字段分组
-		builder.setBolt("areaBolt", new AreaAmtBolt() , 2).fieldsGrouping("filter", new Fields("area_id")) ;
-		//统一处理
-		builder.setBolt("rsltBolt", new AreaRsltBolt(), 1).shuffleGrouping("areaBolt");
+        builder.setSpout("spout", new OrderBaseSpout(KafkaProperties.Order_topic), 5);
+        //随机分组
+        builder.setBolt("filter", new AreaFilterBolt(), 5).shuffleGrouping("spout");
+        //按字段分组
+        builder.setBolt("areaBolt", new AreaAmtBolt(), 2).fieldsGrouping("filter", new Fields("area_id"));
+        //统一处理
+        builder.setBolt("rsltBolt", new AreaRsltBolt(), 1).shuffleGrouping("areaBolt");
+
+        Config conf = new Config();
+        conf.setDebug(true);
+
+        if (args.length > 0) {
+            try {
+                StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            LocalCluster localCluster = new LocalCluster();
+            localCluster.submitTopology("topology", conf, builder.createTopology());
+        }
 
 
-
-		Config conf = new Config() ;
-		conf.setDebug(true);
-
-		if (args.length > 0) {
-			try {
-				StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}else {
-			LocalCluster localCluster = new LocalCluster();
-			localCluster.submitTopology("topology", conf, builder.createTopology());
-		}
-		
-		
-	}
+    }
 
 }
