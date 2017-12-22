0ffae45510e53fce88530de633f18d6f844083d3
seventeen17 <1529068093@qq.com>
2017-12-22 19:44:25 +0800

    commit

--- h-storm-flow/src/main/java/beibei/trident/TridentTopo.java
+++ h-storm-flow/src/main/java/beibei/trident/TridentTopo.java
@@ -7,6 +7,7 @@ import beibei.trident.function.Split;
 import beibei.trident.function.SplitBy;
 import hbase.state.HBaseAggregateState;
 import hbase.state.TridentConfig;
+import kafka.api.OffsetRequest;
 import kafka.productor.KafkaProperties;
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
@@ -28,92 +29,87 @@ import org.apache.storm.trident.operation.builtin.Sum;
 import org.apache.storm.trident.state.StateFactory;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.utils.Utils;
+import tools.DateFmt;
 
 public class TridentTopo {
 
-	public static StormTopology builder(LocalDRPC drpc)
-	{
-		TridentConfig tridentConfig = new TridentConfig("state");
-		StateFactory state = HBaseAggregateState.transactional(tridentConfig);
-		BrokerHosts zkHosts = new ZkHosts(KafkaProperties.zkConnect);
-		String topic = "track";
-		TridentKafkaConfig config = new TridentKafkaConfig(zkHosts, topic);
-		config.scheme = new SchemeAsMultiScheme(new StringScheme());
-		config.fetchSizeBytes = 100 ;//batch size
-		
-		TransactionalTridentKafkaSpout spout  = new TransactionalTridentKafkaSpout(config) ;
-		
-		TridentTopology topology = new TridentTopology() ;
-		//销售额
-		TridentState amtState = topology.newStream("spout", spout)
-		.parallelismHint(3)
-		.each(new Fields(StringScheme.STRING_SCHEME_KEY),new OrderAmtSplit("\\t"), new Fields("order_id","order_amt","create_date","province_id","cf"))
-		.shuffle()
-		.groupBy(new Fields("create_date","cf","province_id"))
-		.persistentAggregate(state, new Fields("order_amt"), new Sum(), new Fields("sum_amt"));
+    public static StormTopology builder(LocalDRPC drpc) {
+        TridentConfig tridentConfig = new TridentConfig("state");
+        StateFactory state = HBaseAggregateState.transactional(tridentConfig);
+        BrokerHosts zkHosts = new ZkHosts(KafkaProperties.zkConnect);
+        String topic = KafkaProperties.Order_topic;
+        TridentKafkaConfig config = new TridentKafkaConfig(zkHosts, topic);
+        config.scheme = new SchemeAsMultiScheme(new StringScheme());
+        config.fetchSizeBytes = 100;//batch size
+        //Todo 版本临时解决方案，后期研究
+        config.startOffsetTime = OffsetRequest.LatestTime();
+        TransactionalTridentKafkaSpout spout = new TransactionalTridentKafkaSpout(config);
+
+        TridentTopology topology = new TridentTopology();
+        //销售额
+        TridentState amtState = topology.newStream("spout", spout)
+                .parallelismHint(3)
+                .each(new Fields(StringScheme.STRING_SCHEME_KEY), new OrderAmtSplit("\\t"), new Fields("order_id", "order_amt", "create_date", "province_id", "cf"))
+                .shuffle()
+                .groupBy(new Fields("create_date", "cf", "province_id"))
+                .persistentAggregate(state, new Fields("order_amt"), new Sum(), new Fields("sum_amt"));
 //		.persistentAggregate(new MemoryMapState.Factory(), new Fields("order_amt"), new Sum(), new Fields("sum_amt"));
-		
-		topology.newDRPCStream("getOrderAmt", drpc)
-		.each(new Fields("args"), new Split(" "), new Fields("arg"))
-		.each(new Fields("arg"), new SplitBy("\\:"), new Fields("create_date","cf","province_id"))
-		.groupBy(new Fields("create_date","cf","province_id"))
-		.stateQuery(amtState, new Fields("create_date","cf","province_id"), new MapGet(), new Fields("sum_amt"))
+
+        topology.newDRPCStream("getOrderAmt", drpc)
+                .each(new Fields("args"), new Split(" "), new Fields("arg"))
+                .each(new Fields("arg"), new SplitBy("\\:"), new Fields("create_date", "cf", "province_id"))
+                .groupBy(new Fields("create_date", "cf", "province_id"))
+                .stateQuery(amtState, new Fields("create_date", "cf", "province_id"), new MapGet(), new Fields("sum_amt"))
 //		.applyAssembly(new FirstN(5, "sum_amt", true))
-		;
-		
-		//订单数
-		TridentState orderState = topology.newStream("orderSpout", spout)
-		.parallelismHint(3)
-		.each(new Fields(StringScheme.STRING_SCHEME_KEY),new OrderNumSplit("\\t"), new Fields("order_id","order_amt","create_date","province_id","cf"))
-		.shuffle()
-		.groupBy(new Fields("create_date","cf","province_id"))
-		.persistentAggregate(state, new Fields("order_id"), new Count(), new Fields("order_num"));
+        ;
+
+        //订单数
+        TridentState orderState = topology.newStream("orderSpout", spout)
+                .parallelismHint(3)
+                .each(new Fields(StringScheme.STRING_SCHEME_KEY), new OrderNumSplit("\\t"), new Fields("order_id", "order_amt", "create_date", "province_id", "cf"))
+                .shuffle()
+                .groupBy(new Fields("create_date", "cf", "province_id"))
+                .persistentAggregate(state, new Fields("order_id"), new Count(), new Fields("order_num"));
 //		.persistentAggregate(new MemoryMapState.Factory(), new Fields("order_id"), new Count(), new Fields("order_num"));
-		
-		topology.newDRPCStream("getOrderNum", drpc)
-		.each(new Fields("args"), new Split(" "), new Fields("arg"))
-		.each(new Fields("arg"), new SplitBy("\\:"), new Fields("create_date","cf","province_id"))
-		.groupBy(new Fields("create_date","cf","province_id"))
-		.stateQuery(orderState, new Fields("create_date","cf","province_id"), new MapGet(), new Fields("order_num"))
+
+        topology.newDRPCStream("getOrderNum", drpc)
+                .each(new Fields("args"), new Split(" "), new Fields("arg"))
+                .each(new Fields("arg"), new SplitBy("\\:"), new Fields("create_date", "cf", "province_id"))
+                .groupBy(new Fields("create_date", "cf", "province_id"))
+                .stateQuery(orderState, new Fields("create_date", "cf", "province_id"), new MapGet(), new Fields("order_num"))
 //		.applyAssembly(new FirstN(5, "order_num", true))
-		;
-		return topology.build() ;
-	}
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LocalDRPC drpc = new LocalDRPC();
-		Config conf = new Config() ;
-		conf.setDebug(false);
-		if (args.length > 0) {
-			try {
-				StormSubmitter.submitTopology(args[0], conf, builder(null));
-			} catch (AlreadyAliveException e) {
-				e.printStackTrace();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}else {
-			LocalCluster localCluster = new LocalCluster();
-			localCluster.submitTopology("mytopology", conf, builder(drpc));
-		}
-		
-		while (true) {
-			System.err.println("销售额："+drpc.execute("getOrderAmt", "2014-08-19:cf:amt_1 2014-08-19:cf:amt_2")) ;
-			System.err.println("订单数："+drpc.execute("getOrderNum", "2014-08-19:cf:orderNum_1 2014-08-19:cf:orderNum_2")) ;
-			Utils.sleep(5000);
-		}
-		/**
-		 * [["2014-08-19:1 2014-08-19:2 2014-08-19:3 2014-08-19:4 2014-08-19:5","2014-08-19:1","2014-08-19","1",821.9000000000001],
-		 * ["2014-08-19:1 2014-08-19:2 2014-08-19:3 2014-08-19:4 2014-08-19:5","2014-08-19:2","2014-08-19","2",631.3000000000001],
-		 * ["2014-08-19:1 2014-08-19:2 2014-08-19:3 2014-08-19:4 2014-08-19:5","2014-08-19:3","2014-08-19","3",240.7],
-		 * ["2014-08-19:1 2014-08-19:2 2014-08-19:3 2014-08-19:4 2014-08-19:5","2014-08-19:4","2014-08-19","4",340.4],
-		 * ["2014-08-19:1 2014-08-19:2 2014-08-19:3 2014-08-19:4 2014-08-19:5","2014-08-19:5","2014-08-19","5",460.8]]
-		 */
-		
-		
-	}
+        ;
+        return topology.build();
+    }
+
+    public static void main(String[] args) {
+
+        LocalDRPC drpc = new LocalDRPC();
+        Config conf = new Config();
+        conf.setDebug(false);
+        if (args.length > 0) {
+            try {
+                StormSubmitter.submitTopology(args[0], conf, builder(null));
+            } catch (AlreadyAliveException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            LocalCluster localCluster = new LocalCluster();
+            localCluster.submitTopology("mytopology", conf, builder(drpc));
+        }
+
+        String countDate = DateFmt.getCountDate(null, DateFmt.date_short);
+
+
+        while (true) {
+            System.err.println("销售额：" + drpc.execute("getOrderAmt", countDate + ":cf:amt_1 " + countDate + ":cf:amt_2 "));
+            System.err.println("订单数：" + drpc.execute("getOrderNum", countDate + ":cf:orderNum_1 " + countDate + ":cf:orderNum_2"));
+            Utils.sleep(5000);
+        }
+
+
+    }
 
 }
