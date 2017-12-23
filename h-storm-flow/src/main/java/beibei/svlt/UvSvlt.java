152a29cf7afe927a7aeea42c28354d1a6dc59a79
seventeen17 <1529068093@qq.com>
2017-12-23 17:28:45 +0800

    commit

--- h-storm-flow/src/main/java/beibei/svlt/UvSvlt.java
+++ h-storm-flow/src/main/java/beibei/svlt/UvSvlt.java
@@ -1,13 +1,5 @@
 package beibei.svlt;
 
-import java.io.IOException;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import beibei.dao.HBaseDAO;
 import beibei.dao.imp.HBaseDAOImp;
 import org.apache.hadoop.hbase.KeyValue;
@@ -15,185 +7,181 @@ import org.apache.hadoop.hbase.client.Result;
 import tools.CommonUtil;
 import tools.DateFmt;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
 public class UvSvlt extends HttpServlet {
 
 
-	private HBaseDAO dao;
+    public String initData = "[]";
+    public String initHisData = "[]";
+    public String initHourData = "[]";
+    String[] colsArr = new String[]{"time_title", "xValue", "uv"};
+    private HBaseDAO dao;
+    private String jsonStr = "";
+    private String todayStr = null;
+    private String xValue = "0";
+    private String xTitle = "0";
+    private String uv = "0";
 
-	public String initData = "[]";
+    @Override
+    public void init() {
+        try {
+            dao = new HBaseDAOImp();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        /** 跨天参数 */
+        todayStr = DateFmt.getCountDate(null, DateFmt.date_short);
+        /** 每次刷新页面更新数据 */
+        initStrData();
+    }
 
-	public String initHisData = "[]";
+    private void initStrData() {
+        //初始化历史点，包括当天和上月
+        try {
+            String rowkey_today = todayStr.replaceAll("-", "");
 
-	public String initHourData = "[]";
-	
-	private String jsonStr = "";
+            String rowkey_his = DateFmt.getCountDate(null, DateFmt.date_short, -1).replaceAll("-", "");
 
-	private String todayStr = null;
+            /** 读取线图当天数据 */
+            List<Result> listToday = dao.getRows(
+                    "uv_table", rowkey_today,
+                    colsArr);
+            initData = CommonUtil.transformHistoryData(listToday, colsArr);
 
-	private String xValue = "0";
+            /** 读取上月同比数据 */
+            List<Result> listYc = dao.getRows(
+                    "uv_table", rowkey_his,
+                    colsArr);
+            initHisData = CommonUtil.transformHistoryData(listYc, colsArr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private String xTitle = "0";
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
 
-	private String uv = "0";
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        try {
+            System.out.println("--------------dopost--------------");
 
-	String [] colsArr = new String[] { "time_title", "xValue", "uv" } ;
-	
-	@Override
-	public void init() {
-		try {
-			dao = new HBaseDAOImp();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		/** 跨天参数 */
-		todayStr = DateFmt.getCountDate(null, DateFmt.date_short);
-		/** 每次刷新页面更新数据 */
-		initStrData();
-	}
+            initHourData = this.getData(todayStr + "_hour", dao);
 
-	private void initStrData() {
-		//初始化历史点，包括当天和上月
-		try {
-			String rowkey_today = todayStr	.replaceAll("-", "");
+            // 读取历史点
+            request.setCharacterEncoding("utf-8");
+            response.setContentType("text/html;charset=utf-8");
+            initStrData();
 
-			String rowkey_his = DateFmt.getCountDate(null, DateFmt.date_short, -1) .replaceAll("-", "");
+            jsonStr = "{\'initData\':" + initData + ",\'initHisData\':"
+                    + initHisData + ",\'initHourData\':'"
+                    + initHourData + "'}";
 
-			/** 读取线图当天数据 */
-			List<Result> listToday = dao.getRows(
-					"uv_table", rowkey_today,
-					colsArr);
-			initData = CommonUtil.transformHistoryData(listToday, colsArr);
+            response.getWriter().write(
+                    "<script type=\"text/javascript\">parent.msg" + "(\""
+                            + jsonStr + "\")</script>");
+            response.flushBuffer();
 
-			/** 读取上月同比数据 */
-			List<Result> listYc = dao.getRows(
-					"uv_table", rowkey_his,
-					colsArr);
-			initHisData = CommonUtil.transformHistoryData(listYc, colsArr);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            System.out.println("initHourData:" + initHourData);
+            System.out.println("initData:" + initData);
+            System.out.println("initHisData:" + initHisData);
+            while (true) {
+                // 每次循环追加一个点
+                // 判断是否跨天
+                if (!DateFmt.getCountDate(null, DateFmt.date_short)
+                        .equals(todayStr)) {
+                    // 跨天初始化
+                    init();
+                    jsonStr = "{\'initData\':" + initData + ",\'initHisData\':"
+                            + initHisData + ",\'isNewDay\':" + 1 + ",\'initHourData\':'"
+                            + initHourData + "'}";
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		this.doPost(req, resp);
-	}
+                    response.setContentType("text/html;charset=utf-8");
+                    response.getWriter().write(
+                            "<script type=\"text/javascript\">parent.msg"
+                                    + "(\"" + jsonStr + "\")</script>");
+                    response.flushBuffer();
+                }
 
-	@Override
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		try {
-			System.out.println("--------------dopost--------------");
-			
-			initHourData = this.getData(todayStr+"_hour", dao);
-			
-			// 读取历史点
-			request.setCharacterEncoding("utf-8");
-			response.setContentType("text/html;charset=utf-8");
-			initStrData();
+                /** 读取hbase数据 */
+                String[] valueArr = null;
+                valueArr = CommonUtil.findLineData(dao,
+                        "uv_table", todayStr + "_lastest",
+                        colsArr);
+                if (valueArr != null) {
+                    xTitle = valueArr[0];
+                    xValue = valueArr[1];
+                    uv = valueArr[2];
+                }
 
-			jsonStr = "{\'initData\':" + initData + ",\'initHisData\':"
-					+ initHisData + ",\'initHourData\':'"
-					+ initHourData + "'}";
-			
-			response.getWriter().write(
-					"<script type=\"text/javascript\">parent.msg" + "(\""
-							+ jsonStr + "\")</script>");
-			response.flushBuffer();
-			
-			System.out.println("initHourData:"+initHourData);
-			System.out.println("initData:"+initData);
-			System.out.println("initHisData:"+initHisData);
-			while (true) {
-				// 每次循环追加一个点
-				// 判断是否跨天
-				if (! DateFmt.getCountDate(null, DateFmt.date_short)
-						.equals(todayStr)) {
-					// 跨天初始化
-					init();
-					jsonStr = "{\'initData\':" + initData + ",\'initHisData\':"
-							+ initHisData + ",\'isNewDay\':" + 1 + ",\'initHourData\':'"
-							+ initHourData + "'}";
-
-					response.setContentType("text/html;charset=utf-8");
-					response.getWriter().write(
-							"<script type=\"text/javascript\">parent.msg"
-									+ "(\"" + jsonStr + "\")</script>");
-					response.flushBuffer();
-				}
-
-				/** 读取hbase数据 */
-				String[] valueArr = null;
-				valueArr = CommonUtil.findLineData(dao,
-						"uv_table", todayStr + "_lastest",
-						colsArr);
-				if (valueArr != null) {
-					xTitle = valueArr[0];
-					xValue = valueArr[1];
-					uv = valueArr[2];
-				}
-
-				//加点
+                //加点
 //				initData = CommonUtil.appendStr(initData, colsArr);
 
-				jsonStr = "{\'name\':\'" + xTitle + "\',\'x\':" + xValue
-						+ ",\'y\':" + uv + ",\'initHourData\':'"
-						+ initHourData+ "'}";
-				
-				/** 判断一下如果值不为null才sendMsg */
-				if (uv != null) {
-					boolean b = sendMsg(jsonStr, response, "msg"); 
-					if (b) {// 页面已被关闭，加点异常，此方法结束
-						break;
-					}
-				}
-				// 5秒推送一次数据
-				Thread.sleep(5000);
-				initHourData = this.getData(todayStr+"_hour", dao);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                jsonStr = "{\'name\':\'" + xTitle + "\',\'x\':" + xValue
+                        + ",\'y\':" + uv + ",\'initHourData\':'"
+                        + initHourData + "'}";
 
-	// 以下方法的意思是将msg打到前台页面调用前台的“function msg(m)”方法进行显示
-	protected boolean sendMsg(String msg, HttpServletResponse response,
-			String javascriptMethod) {
-		try {
-			response.setContentType("text/html;charset=utf-8");
-			response.getWriter().write(
-					"<script type=\"text/javascript\">parent."
-							+ javascriptMethod + "(\"" + msg + "\")</script>");
-			response.flushBuffer();
+                /** 判断一下如果值不为null才sendMsg */
+                if (uv != null) {
+                    boolean b = sendMsg(jsonStr, response, "msg");
+                    if (b) {// 页面已被关闭，加点异常，此方法结束
+                        break;
+                    }
+                }
+                // 5秒推送一次数据
+                Thread.sleep(5000);
+                initHourData = this.getData(todayStr + "_hour", dao);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-		} catch (Exception e) {
-			System.out.println("svlt long connect time error.....");
-			return true;
-		}
-		return false;
-	}
-	public String getData(String date, HBaseDAO dao)
-	{
-		List<Result> list =  dao.getRows("uv_table", date) ;
-		String result = "[" ;
-		for(Result rs : list)
-		{
-			for(KeyValue keyValue : rs.raw())
-			{
-				if("uv".equals(new String(keyValue.getQualifier())))
-				{
-					result += new String(keyValue.getValue())+",";
-					break;
-				}
-			}
-		}
-		result = result.substring(0, result.length() -1 ) +"]" ;
-		return result;
-		
-	}
-	@Override
-	public void destroy() {
-		super.destroy();
-	}
+    // 以下方法的意思是将msg打到前台页面调用前台的“function msg(m)”方法进行显示
+    protected boolean sendMsg(String msg, HttpServletResponse response,
+                              String javascriptMethod) {
+        try {
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().write(
+                    "<script type=\"text/javascript\">parent."
+                            + javascriptMethod + "(\"" + msg + "\")</script>");
+            response.flushBuffer();
+
+        } catch (Exception e) {
+            System.out.println("svlt long connect time error.....");
+            return true;
+        }
+        return false;
+    }
+
+    public String getData(String date, HBaseDAO dao) {
+        List<Result> list = dao.getRows("uv_table", date);
+        String result = "[";
+        for (Result rs : list) {
+            for (KeyValue keyValue : rs.raw()) {
+                if ("uv".equals(new String(keyValue.getQualifier()))) {
+                    result += new String(keyValue.getValue()) + ",";
+                    break;
+                }
+            }
+        }
+        result = result.substring(0, result.length() - 1) + "]";
+        return result;
+
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
 }
